24173be5107f0f690dd88c2f6cbb52a5536430a2
Bikram Dahal <bkrm.dahal@gmail.com>
2016-02-01 00:01:05 +0545

    v3

--- WBS - Part 2 - Data Data Visualization(Public Sector: Defense & arms trade).R
+++ WBS - Part 2 - Data Data Visualization(Public Sector: Defense & arms trade).R
@@ -20,8 +20,6 @@ setwd("M:/")
 filepath=getwd()
 setwd(paste(filepath, "R_Script/Combine", sep="/"))
 
-
-
 #####readfile from your directory
 
 wdi = read_csv("WDI_Data.csv")
@@ -33,7 +31,7 @@ i_name= read_csv("WDI_Series.csv")
 #### create subset of above data, select only required row
 ##select only data after 1980 and required col from wdi
 
-wdi_sub = wdi[ , c(1,3,25:60)]
+wdi_sub = wdi[ , c(1,3,5:60)]
 
 
 ##lets run anysis on country name only; country name in wdi file has other names like summary of region
@@ -42,30 +40,20 @@ country_sub = subset(country, country$`Currency Unit`!="" ,
                      select = c("Table Name", "Region")) # if currency unit is blank its not country
 colnames(country_sub) <- c("Country Name", "Region")
 
+
+
 ##lets get only one Topic
+
  i_name_sub = subset(i_name, i_name$Topic=="Public Sector: Defense & arms trade", select="Indicator Name")
 
-##OR get particular Indicator Name
 
-#to remove multiple comment just select line click shif+ctrl+c
-i_name_sub = subset(i_name, i_name$`Indicator Name`=="Foreign direct investment, net (BoP, current US$)" |
-                      i_name$`Indicator Name`=="GDP growth (annual %)" | 
-                      i_name$`Indicator Name`=="Population density (people per sq. km of land area)" |
-                      i_name$`Indicator Name`=="CO2 emissions (kt)" |
-                      i_name$`Indicator Name`=="Access to electricity (% of population)" |
-                      i_name$`Indicator Name`=="Forest area (% of land area)" |
-                      i_name$`Indicator Name`=="Inflation, consumer prices (annual %)" |
-                      i_name$`Indicator Name`=="Life expectancy at birth, total (years)" |
-                      i_name$`Indicator Name`=="Birth rate, crude (per 1,000 people)" , select="Indicator Name")
-
-####let join all data to get only required data
 
 wdi_sub = left_join(country_sub, wdi_sub)
 wdi_sub = left_join(i_name_sub, wdi_sub)
 
 ####lets gather all data into narrow form 
 
-wdi_sub = gather(wdi_sub, "years", "sample", 4:39)
+wdi_sub = gather(wdi_sub, "years", "sample", 4:59)
 
 #### Rename for easy handling
 
@@ -88,12 +76,10 @@ M = gvisMotionChart(wdi_sub, idvar = "Country.Name", timevar = "years", colorvar
                     options = list(width = 700, height = 600), chartid= "country_gdp")
 
 plot(M)
-
-###Save the file 
 setwd(filepath)
 dir.create("Output")
 setwd(paste(filepath, "Output", sep="/"))
-write.csv(wdi_sub, file="seller_top_level_status.csv")
+write.csv(wdi_sub, file="Public Sector: Defense & arms trade.csv")
 setwd(filepath)
 rm(list=ls())
 
